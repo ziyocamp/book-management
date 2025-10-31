60ab07d0acd0255b83738c47aa7a955b69df9309
djumanov <djumanovdev@gmail.com>
2025-10-31 18:20:41 +0500

    done

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home_view, books_view, book_detail_view
+from .views import home_view, books_view, book_detail_view, add_book_view
 
 app_name = "books"
 
 urlpatterns = [
     path('', home_view, name='home_page'),
     path('books/', books_view, name='books_page'),
-    path('books/<int:book_id>', book_detail_view, name='deatil'),
+    path('books/<int:book_id>', book_detail_view, name='detail'),
+    path('books/add/', add_book_view, name='add_book')
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpRequest, HttpResponse
 
 
@@ -6,12 +7,18 @@ books = [
     {
         'id': 1,
         'name': 'O\'tgan kunlar',
-        'author': 'Abudlla Qodiriy'
+        'author': 'Abudlla Qodiriy',
+        'publication_year': 2000,
+        'status': 'available',
+        'isbn': 324123,
     },
     {
         'id': 2,
         'name': 'Hamsa',
-        'author': 'Alisher Navoiy'
+        'author': 'Alisher Navoiy',
+        'publication_year': 2000,
+        'status': 'available',
+        'isbn': 43241234,
     },
 ]
 
@@ -21,6 +28,10 @@ def home_view(request: HttpRequest) -> HttpResponse:
 def books_view(request: HttpRequest) -> HttpResponse:
     context = {
         'books': books,
+        'total_books': len(books),
+        'available_books': 1231,
+        'borrowed_books': 312,
+        'authors_count': 231
     }
     return render(request=request, template_name='books.html', context=context)
 
@@ -33,4 +44,24 @@ def book_detail_view(request: HttpRequest, book_id: int) -> HttpResponse:
             return render(request=request, template_name='book_detail.html', context=context)
         
     return render(request=request, template_name='book.html')
+
+def add_book_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return render(request=request, template_name='add_book.html')
+
+    elif request.method == 'POST':
+        form = request.POST
+
+        print(form)
+
+        new_book = {
+            'id': 3,
+            'name': form.get('name'),
+            'author': form.get('author'),
+            'publication_year': form.get('publication_year', 2000),
+            'status': form.get('status', 'availabe'),
+            'isbn': form.get('isbn')
+        }
+        books.append(new_book)
+        return redirect(to=reverse('books:books_page'))
 
